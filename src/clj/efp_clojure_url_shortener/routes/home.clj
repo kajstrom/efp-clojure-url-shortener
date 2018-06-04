abc4bdb74d630705ddbac2148102aaa24fc4f269
Kaj Ström <kaj.strom@kapsi.fi>
2018-06-04 19:32:49 +0300

    Replace default home page content with form

--- src/clj/efp_clojure_url_shortener/routes/home.clj
+++ src/clj/efp_clojure_url_shortener/routes/home.clj
@@ -1,17 +1,22 @@
 (ns efp-clojure-url-shortener.routes.home
   (:require [efp-clojure-url-shortener.layout :as layout]
-            [compojure.core :refer [defroutes GET]]
+            [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :as response]
             [clojure.java.io :as io]))
 
 (defn home-page []
   (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+    "home.html"))
+
+(defn add-url [request]
+  (println request)
+  (response/found "/"))
 
 (defn about-page []
   (layout/render "about.html"))
 
 (defroutes home-routes
   (GET "/" [] (home-page))
+  (POST "/add" request (add-url request))
   (GET "/about" [] (about-page)))
 
